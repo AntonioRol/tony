cd01a2401cb3f255d9a064b7bcd193b0d99eb261
Antonio Roldan <Antonio Roldan>
2016-04-24 20:17:43 -0400

    Initial commit

---
+++ prueba 18.py
@@ -0,0 +1,13 @@
+n = 10
+m = 20
+p = 30
+if n <= m:
+    print "n es menor o igual que m"
+else:
+    print "n no es menor o igual que m"
+if m <= p:
+    print "m es menor o igual que p"
+else:
+    print "m no es menor o igual que p"
+
+
